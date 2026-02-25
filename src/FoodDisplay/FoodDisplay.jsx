98f99ab02a11e853fb4ecde2dac46f5a47c16bc9
Sriman Narayana <154743522+Narayanasriman@users.noreply.github.com>
2026-02-25 15:01:28 +0530

    Update FoodDisplay.jsx

--- src/FoodDisplay/FoodDisplay.jsx
+++ src/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import './FoodDisplay.css';
 import { StoreContext } from '../Context/StoreContext';
-import Fooditem from '../components/Fooditem/Fooditem';
+import Fooditem from '../components/FoodItem/FoodItem';
 
 const FoodDisplay = ({category}) => {
     const { food_list } = useContext(StoreContext);
